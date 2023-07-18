1fe9baeb7a060bf3029be68e785a3e34ee227763
ElizabethT7 <tymofieievai@gmail.com>
2023-07-18 16:28:50 +0300

    feat: add start settings

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    Work hello !
+    <button>Click me!</button>
+    <button v-on:click='addLike'>Like</button>
+    <button @click='addDislike'>Dislike</button>
+    <div>Likes count is <strong>{{ likes }}</strong></div>
+    <div>Dislikes count is <strong>{{ dislikes }}</strong></div>
+    <input type='text'>
+    <textarea name='' cols='30' rows='10'></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 5,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike(){
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App';
+
+/*const App = {
+  data,
+  methods
+}*/
+
+createApp(App).mount('#app');
